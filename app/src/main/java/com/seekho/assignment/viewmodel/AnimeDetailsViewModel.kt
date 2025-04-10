5dbfd0600d10271fa9207171d0acd458779b778b
Dhruv Patel <dhruvp5200@gmail.com>
2025-04-10 22:41:36 +0530

    Implemented Anime Details ViewModel

---
+++ app/src/main/java/com/seekho/assignment/viewmodel/AnimeDetailsViewModel.kt
@@ -0,0 +1,27 @@
+package com.seekho.assignment.viewmodel
+
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.seekho.assignment.model.AnimeDetails
+import com.seekho.assignment.network.RetrofitService
+import com.seekho.assignment.repository.AnimeDetailsRepository
+import com.seekho.assignment.repository.AnimeDetailsRepositoryImpl
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class AnimeDetailsViewModel:ViewModel() {
+    private val _animeDetails = MutableStateFlow(AnimeDetails())
+    private val repository:AnimeDetailsRepository = AnimeDetailsRepositoryImpl(RetrofitService.apiService)
+    val animeDetails:StateFlow<AnimeDetails>
+        get() = _animeDetails
+
+    fun fetchAnimeDetails(){
+        viewModelScope.launch{
+            repository.fetchAnimeDetails().collect{
+                _animeDetails.emit(it)
+            }
+        }
+    }
+}
